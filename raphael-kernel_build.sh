75784793ff0b9072d16319a1ff5170f850d79526
GengWei1997 <1638044635@qq.com>
2026-02-21 11:20:56 +0800

    1

---
+++ raphael-kernel_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e  # 遇到错误立即退出
+
+# 克隆指定版本的内核源码
+git clone https://github.com/GengWei1997/linux.git --branch raphael-$1 --depth 1 linux
+
+# 内核配置文件
+mv uboot-raphael.config arch/arm64/configs/raphael.config
+
+# 应用 builddeb 补丁
+patch linux/scripts/package/builddeb < builddeb.patch
+
+cd linux
+git add .
+git commit -m "builddeb: Add Qcom SM8150 DTBs to boot partition"
+
+# 生成内核配置
+make -j$(nproc) ARCH=arm64 LLVM=-21 defconfig raphael.config
+cp .config ../raphael.config
+
+# 清理源码目录
+rm -rf linux
