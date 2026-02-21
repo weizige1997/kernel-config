6ddf42fd3ea2bb7644f33c83b7d839ae4ee86546
GengWei1997 <1638044635@qq.com>
2026-02-21 11:22:15 +0800

    2

--- raphael-kernel_build.sh
+++ raphael-kernel_build.sh
@@ -2,7 +2,8 @@
 set -e  # 遇到错误立即退出
 
 # 克隆指定版本的内核源码
-git clone https://github.com/GengWei1997/linux.git --branch raphael-$1 --depth 1 linux
+#git clone https://github.com/GengWei1997/linux.git --branch sm8150/$1 --depth 1 linux
+git clone https://github.com/Aospa-raphael-unofficial/linux.git --branch sm8150/$1 --depth 1 linux
 
 # 内核配置文件
 mv uboot-raphael.config arch/arm64/configs/raphael.config
