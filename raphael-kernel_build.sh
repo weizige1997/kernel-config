2e5fdd8900c2cec08eb367ae8c6b7ad4ac3b7573
GengWei1997 <1638044635@qq.com>
2026-04-24 09:14:40 +0800

    update

--- raphael-kernel_build.sh
+++ raphael-kernel_build.sh
@@ -2,8 +2,8 @@
 set -e  # 遇到错误立即退出
 
 # 克隆指定版本的内核源码
-#git clone https://github.com/GengWei1997/linux.git --branch sm8150/$1 --depth 1 linux
-git clone https://github.com/Aospa-raphael-unofficial/linux.git --branch sm8150/$1 --depth 1 linux
+git clone https://github.com/GengWei1997/linux.git --branch raphael-$1 --depth 1 linux
+#git clone https://github.com/Aospa-raphael-unofficial/linux.git --branch sm8150/$1 --depth 1 linux
 
 # 内核配置文件
 mv uboot-raphael.config linux/arch/arm64/configs/raphael.config
@@ -16,7 +16,7 @@ git add .
 git commit -m "builddeb: Add Qcom SM8150 DTBs to boot partition"
 
 # 生成内核配置
-make -j$(nproc) ARCH=arm64 LLVM=-21 defconfig raphael.config
+make -j$(nproc) ARCH=arm64 LLVM=-22 defconfig raphael.config
 cp .config ../raphael.config
 
 # 清理源码目录
